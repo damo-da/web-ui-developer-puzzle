7f27cb0f922b85d6ba54a4ef930df70f82cce21b
Damodar Dahal <underscoredam@gmail.com>
2020-07-12 18:46:53 -0700

    add undo feature

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -4,11 +4,12 @@ import {
   addToReadingList,
   clearSearch,
   getAllBooks,
-  ReadingListBook,
+  ReadingListBook, removeFromReadingList,
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
-import { Book } from '@tmo/shared/models';
+import { Book, ReadingListItem } from '@tmo/shared/models';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'tmo-book-search',
@@ -24,7 +25,8 @@ export class BookSearchComponent implements OnInit {
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private readonly snackBar: MatSnackBar,
   ) {}
 
   get searchTerm(): string {
@@ -45,6 +47,16 @@ export class BookSearchComponent implements OnInit {
 
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
+
+    this.snackBar.open(`Adding ${book.title} to reading list.`, 'Undo', {
+      duration: 3000,
+    })
+      .onAction()
+      .subscribe(() => {
+        const readingListItem = <ReadingListItem> { bookId: book.id, ...book };
+        this.store.dispatch(removeFromReadingList({ item: readingListItem }));
+      });
+
   }
 
   searchExample() {
